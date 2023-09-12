526e32de94de62cb31bfc6e87dc1f90c34d15eb3
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-09-12 14:16:15 -0400

    Update productsCtrl.js

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -14,10 +14,10 @@ const show = (req, res, next) => {
 };
 
 const form = (req, res, next) => {
-  // res.send(`Product.form`);
-
+  const id = Number(req.params.id);
+  const product = products.find((p) => p.id === id);
   if (req.params.id) {
-    res.render('views/products/edit.html.twig', { product });
+    res.render('views/products/edit.html.twig', { product, id });
   } else {
     res.render('views/products/create.html.twig');
   }
@@ -31,11 +31,17 @@ const create = (req, res, next) => {
 };
 
 const update = (req, res, next) => {
-  const product = Products.update(req.params.id, req.body);
-  res.redirect(`/products/${req.params.id}`);
+  const id = Number(req.params.id);
+  products.map((p) => {
+    if (p.id === id) {
+      return req.body;
+    }
+  });
+  res.redirect(`/products/${id}`);
 };
 
 const remove = (req, res, next) => {
+  products = products.filter((p) => p.id !== id);
   res.redirect('/products');
 };
 
